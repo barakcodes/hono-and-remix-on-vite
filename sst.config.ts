985ccef8d03b36c6249ab6e516c9e215a69bdb0c
barakcodes <35469348+barakcodes@users.noreply.github.com>
2024-09-03 13:21:55 +0300

    Update sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -1,10 +1,6 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
-let stage: string | undefined
-
 export default $config({
   app(input) {
-    stage = input?.stage
     return {
       name: 'other',
       removal: input?.stage === 'production' ? 'retain' : 'remove',
@@ -14,9 +10,8 @@ export default $config({
   async run() {
     new sst.aws.Remix('MyWeb', {
       environment: {
-        NODE_ENV: (stage!=='production'&& stage!=='development') ? 'development' : 'production',
+         NODE_ENV: $dev ? "development" : "production",
       },
-      buildCommand: 'bun run build',
       transform: {
         server: {
           handler: 'functions/server.handler',
